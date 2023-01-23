9e8e18e8eae588d8d4d37ad2b3f1c4e47e705ea5
Julia <julia.masaeva@gmail.com>
2023-01-23 22:03:17 +0600

    Найти сумму чисел от 1 до А

---
+++ Task002/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.Write("Введите А: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+int result = (number*(number + 1) / 2);
+Console.WriteLine(result);
+
