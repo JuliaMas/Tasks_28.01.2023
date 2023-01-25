43d8b0c2147df89721fd3ad710aabb2cf0315d47
Julia <julia.masaeva@gmail.com>
2023-01-25 19:28:55 +0600

    Найти сумму чисел от 1 до А_2

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -2,6 +2,14 @@
 
 Console.Write("Введите А: ");
 int number = int.Parse(Console.ReadLine() ?? "");
+/*int i;
+int result = 0;
+for (i = 1; i<= number; i++)
+{
+      result = result + i;
+      
+}*/
+
 
 int result = (number*(number + 1) / 2);
 Console.WriteLine(result);
