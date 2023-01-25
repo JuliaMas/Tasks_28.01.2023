6e61baa78b548dab3ee34745d41ed2c428925959
Julia <julia.masaeva@gmail.com>
2023-01-25 20:47:51 +0600

    вычисление произведения чисел от 1 до N

---
+++ Task005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите А: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+int i;
+int result = 1;
+for (i = 1; i<= number; i++)
+{
+      result = result * i;
+      
+}
+Console.WriteLine(result);
+
