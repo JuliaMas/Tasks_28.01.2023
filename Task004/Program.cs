ea43cbad66cbb1b4cdcd24e9a9463d7376c7c1ef
Julia <julia.masaeva@gmail.com>
2023-01-25 20:37:50 +0600

    Подсчитать сумму цифр в числе

---
+++ Task004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int sum = 0;
+while (N > 0)
+{
+    int a = N % 10;
+    sum = sum + a;
+    N = N / 10;
+}
+Console.WriteLine("Сумма цифр в числе равна " + sum);
+
+
