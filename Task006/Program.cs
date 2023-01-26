b2899cbb9702a5865752e769eda65e2576613aa9
Julia <julia.masaeva@gmail.com>
2023-01-26 21:03:11 +0600

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ Task006/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+var r = new Random();
+int count = 13;
+int[]array = new int[count]; 
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = r.Next(1, 20);
+}
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if((array[i] % 2) == 0)
+    {
+        int mult = array[i]*array[i]*array[i];
+        Console.WriteLine("куб числа " + array[i] + " равен " + mult);
+    }
+}
