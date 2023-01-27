1b32d2662b74f9cd643617f4f650798bc5ee0ec6
Julia <julia.masaeva@gmail.com>
2023-01-27 20:27:02 +0600

    Определить, присутствует ли в заданном массиве, некоторое число

--- Task009/Program.cs
+++ Task009/Program.cs
@@ -4,7 +4,8 @@ int number = int.Parse(Console.ReadLine());
 int[] array = {10, 93, 48, 46, 77, 48, 92, 14, 76};
 int n = array.Length;
 int i = 0;
-while (i < n)
+int x = 0;
+/*while (i < n)
 {
     if(array[i] == number)
     {
@@ -12,4 +13,15 @@ while (i < n)
         break;
     }    
         i++;
+}*/
+while (i < n)
+{
+    if(array[i] == number)
+    {
+        x = 1;
+    }  
+        i++;
 }
+if (x == 1) Console.WriteLine("Искомое число " + number + " присутствует в массиве");
+else Console.WriteLine("Искомое число " + number + " отсутствует в массиве");
+      
