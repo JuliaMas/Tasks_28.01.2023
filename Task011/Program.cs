e79d61a8d33fd418dfdc7333d5e55e8ccd440e37
Julia <julia.masaeva@gmail.com>
2023-01-28 19:37:14 +0600

    В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]

---
+++ Task011/Program.cs
@@ -0,0 +1,27 @@
+﻿//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int a = 10; //начало отрезка
+int b = 99; // конец отрезка
+int countNum = 0; // количество элементов в отрезке
+int[] array = new int[123];
+
+var r = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = r.Next(a/2, b*2 +1);
+}
+foreach (int j in array)
+{
+    Console.Write(j + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    if (a <= array[i] && array[i] <= b) 
+    {
+        countNum = countNum + 1;
+    }
+}
+Console.WriteLine("Количество элементов в интервале равно " + countNum);
+
+
